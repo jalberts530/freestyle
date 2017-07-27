6cf8b1923ad07c82574c2613037ca12902b0e017
Alberts <jalberts@deloitte.com>
2017-07-26 20:46:36 -0400

    Finalized Price Lookup Operation

--- app/freestyle_app.py
+++ app/freestyle_app.py
@@ -10,7 +10,6 @@ csv_file_path = "data/stockprice.csv"
 my_stocks = []
 stock_symbol = []
 us_market_indexes = ["^DJI", "^GSPC", "^IXIC"]
-#test = ['AAPL','T','GOOGL'] Testing IDs
 
 #MENU
 print("---------------------------------------")
@@ -18,7 +17,7 @@ print("Welcome to Alberts Financial Management")
 print("---------------------------------------")
 print("Date: " + datetime.datetime.now().strftime("%Y-%m-%d") + " " + "Time: " + datetime.datetime.now().strftime("%H:%m:%S"))
 print("\n")
-print("        ~ MENU ~")
+print("         ~MENU~")
 print("Operations   Description")
 print("MARKET     | US Market Summary")
 print("PRICE      | Look up a Stock Price")
@@ -27,13 +26,24 @@ print("PORTFOLIO  | Determine Current Portfolio Value")
 print("---------------------------------------")
 print("Disclaimer: All Data Provided by Yahoo & Google Finance")
 print("\n")
+
 chosen_operation = input("Please input an operation from the menu: ")
 chosen_operation = chosen_operation.title()
+
+while True:
+    chosen_operation
+    if chosen_operation == "EXIT":
+        break
+    else:
+        break
 print(chosen_operation)
 
 #Operation Definitions
+def operation_exit(): # Exit Program
+    print("Goodbye! Come back soon!")
+
 def operation_market():
-    print("\n" + "~ US Stock Market Index Summary Past 3 Days ~")
+    print("\n" + "~US Stock Market Index Summary Past 3 Days~")
     #Data Source from Panda Reader
     data_source = 'yahoo'
     start = str(date.today() - timedelta(days=3)) #> '2017-07-09'
@@ -42,7 +52,7 @@ def operation_market():
     daily_closing_prices = response.ix["Close"] # ix() is a pandas DataFrame function
     print(daily_closing_prices) #figure how to format 2 decimal places
 
-def operation_price():
+def operation_price(): #Show Stock Price for Past 5 Days
     print("Lookup stock price")
     while True:
         stock_lookup = input("Please input a valid Stock Symbol or DONE to exit: ")
@@ -51,17 +61,14 @@ def operation_price():
             break
         else:
             stock_symbol.append(stock_lookup)
-
     #Data Source from Panda Reader
     data_source = 'google'
-    start = str(date.today() - timedelta(days=1)) #> '2017-07-09'
+    start = str(date.today() - timedelta(days=6)) #> '2017-07-09'
     end = str(date.today()) #> '2017-07-24'
-    response = data.DataReader(stock_symbol, data_source, end)
+    response = data.DataReader(stock_symbol, data_source, start, end)
     daily_closing_prices = response.ix["Close"] # ix() is a pandas DataFrame function
-
+    print("~Closing Prices Past 5 Days~")
     print(daily_closing_prices)
-
-
 
 
 def operation_invest():
@@ -97,8 +104,9 @@ if chosen_operation == "Market": operation_market()
 elif chosen_operation == "Price": operation_price()
 elif chosen_operation == "Invest": operation_invest()
 elif chosen_operation == "Portfolio": operation_portfolio()
+elif chosen_operation == "Exit": operation_exit()
 #elif chosen_operation == "Destroy": destroy_product()
-else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.") #insert reset ability
+else: print("OOPS. PLEASE CHOOSE ONE OF THE RECOGNIZED OPERATIONS.")
 
 
 #packages to consider
